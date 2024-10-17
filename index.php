1a078e6b424b1a1474d59874713ec6c1d303c27e
carlos arrarte <arrarte82@gmail.com>
2024-10-16 22:16:04 -0500

    cambios login

--- index.php
+++ index.php
@@ -940,78 +940,31 @@ include ('layout/header.php');
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body" style="width: 600px;background: #fff;">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-6">
-                   <br><br>
-                    <div id="container-form">
-                    
-                       <p style="color:#fff;" class="text-center lead">Debera de llenar todos los campos para registrarse</p>
-                       <br><br>
-                       <form class="form-horizontal FormCatElec" action="process/regclien.php" role="form" method="post" data-form="save">
-                           <div class="form-group">
-                              <div class="input-group">
-                                <div class="input-group-addon"><i class="fa fa-credit-card"></i></div>
-                                <input class="form-control all-elements-tooltip" type="text" placeholder="Ingrese su número de DNI" required name="clien-nit" data-toggle="tooltip" data-placement="top" title="Ingrese su número de DNI." maxlength="30" pattern="[0-9-]{14,30}">
-                              </div>
-                            </div>
-                            <br>
-                            <div class="form-group">
-                              <div class="input-group">
-                                <div class="input-group-addon"><i class="fa fa-user"></i></div>
-                                <input class="form-control all-elements-tooltip" type="text" placeholder="Ingrese su nombre de usuario" required name="clien-name" data-toggle="tooltip" data-placement="top" title="Ingrese su nombre. Máximo 9 caracteres (solamente letras)" pattern="[a-zA-Z]{1,9}" maxlength="9">
-                              </div>
-                            </div>
-                            <br>
-                            <div class="form-group">
-                              <div class="input-group">
-                                <div class="input-group-addon"><i class="fa fa-user"></i></div>
-                                <input class="form-control all-elements-tooltip" type="text" placeholder="Ingrese sus nombres" required name="clien-fullname" data-toggle="tooltip" data-placement="top" title="Ingrese sus nombres.(solamente letras)" pattern="[a-zA-Z ]{1,50}" maxlength="50">
-                              </div>
-                            </div>
-                            <br>
-                            <div class="form-group">
-                              <div class="input-group">
-                                <div class="input-group-addon"><i class="fa fa-user"></i></div>
-                                <input class="form-control all-elements-tooltip" type="text" placeholder="Ingrese sus apellidos" required name="clien-lastname" data-toggle="tooltip" data-placement="top" title="Ingrese sus apellido(solamente letras)" pattern="[a-zA-Z ]{1,50}" maxlength="50">
-                              </div>
-                            </div>
-                            <br>
-                            <div class="form-group">
-                              <div class="input-group">
-                                <div class="input-group-addon"><i class="fa fa-lock"></i></div>
-                                <input class="form-control all-elements-tooltip" type="password" placeholder="Introdusca una contraseña" required name="clien-pass" data-toggle="tooltip" data-placement="top" title="Defina una contraseña para iniciar sesión">
-                              </div>
-                            </div>
-                            <br>
-                            <div class="form-group">
-                              <div class="input-group">
-                                <div class="input-group-addon"><i class="fa fa-home"></i></div>
-                                <input class="form-control all-elements-tooltip" type="text" placeholder="Ingrese su dirección" required name="clien-dir" data-toggle="tooltip" data-placement="top" title="Ingrese la direción en la reside actualmente" maxlength="100">
-                              </div>
-                            </div>
-                            <br>
-                            <div class="form-group">
-                              <div class="input-group">
-                                <div class="input-group-addon"><i class="fa fa-mobile"></i></div>
-                                <input class="form-control all-elements-tooltip" type="tel" placeholder="Ingrese su número telefónico" required name="clien-phone" maxlength="11" pattern="[0-9]{8,11}" data-toggle="tooltip" data-placement="top" title="Ingrese su número telefónico. Mínimo 8 digitos máximo 11">
-                              </div>
-                            </div>
-                            <br>
-                            <div class="form-group">
-                              <div class="input-group">
-                                <div class="input-group-addon"><i class="fa fa-at"></i></div>
-                                <input class="form-control all-elements-tooltip" type="email" placeholder="Ingrese su Email" required name="clien-email" data-toggle="tooltip" data-placement="top" title="Ingrese la dirección de su Email" maxlength="50">
-                              </div>
-                            </div>
-                            <br>
-                            <p><button type="submit" class="btn btn-success btn-block"><i class="fa fa-pencil"></i>&nbsp; Registrarse</button></p>
-                            <div class="ResForm" style="width: 100%; color: #fff; text-align: center; margin: 0;"></div>
-                        </form> 
-                    </div> 
-                </div>
-            </div>
-        </div>  
+        
+        <div class="login-container">
+    <div class="neon-frame">
+        <div class="neon-corner top-left"></div>
+        <div class="neon-corner top-right"></div>
+        <div class="neon-corner bottom-left"></div>
+        <div class="neon-corner bottom-right"></div>
+    </div>
+    <h2>Login</h2>
+    <form>
+        <div class="input-box">
+            <input type="text" required>
+            <label>Username</label>
+        </div>
+        <div class="input-box">
+            <input type="password" required>
+            <label>Password</label>
+        </div>
+        <button type="submit" class="neon-button">Sign In</button>
+        <div class="login-links">
+            <a href="#">Forgot Password?</a>
+            <a href="#">Sign Up</a>
+        </div>
+    </form>
+</div>
     </div>
         </div>
         <div class="modal-footer" style="width: 600px;background: #fff;">
