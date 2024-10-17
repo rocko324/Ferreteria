be78827ec6968558130d6751c64ad3a11e0a881c
carlos arrarte <arrarte82@gmail.com>
2024-10-17 00:07:47 -0500

    cambios login7

--- index.php
+++ index.php
@@ -899,7 +899,7 @@ include ('layout/header.php');
 
   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabe" aria-hidden="true">
     <div class="modal-dialog">
-      <div class="modal-content">
+      <div class="modal-content"  style="width: 70%;">
         <div class="modal-header">
           
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -931,8 +931,8 @@ include ('layout/header.php');
 </div> 
     </div>
         </div>
-        <div class="modal-footer" >
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <div class="modal-footer"  style="width: 350px;">
+        
           
         </div>
       </div>
