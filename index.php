29b7321758d85b2441bffd57ed2e695280b01716
carlos arrarte <arrarte82@gmail.com>
2024-10-16 22:49:24 -0500

    cambios login4

--- index.php
+++ index.php
@@ -899,11 +899,11 @@ include ('layout/header.php');
   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
-        <div class="modal-header" style="width: 600px;background: #fff;">
+        <div class="modal-header" style="width: 400px;background: #fff;">
           
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <div class="modal-body" style="width: 600px;background: #fff;">
+        <div class="modal-body" style="width: 400px;background: #fff;">
           
         <div class="login-container">
     <div class="neon-frame">
@@ -932,7 +932,7 @@ include ('layout/header.php');
         
     </div>
         </div>
-        <div class="modal-footer" style="width: 600px;background: #fff;">
+        <div class="modal-footer" style="width: 400px;background: #fff;">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
           
         </div>
